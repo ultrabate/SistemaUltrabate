5c3a944b28671258a07980fab1b7d1d72ba3e09e
Denisio <Denisio@Denisio>
2013-05-25 11:50:01 -0300

    Colocando o projeto no git

---
+++ src/java/br/com/ultrabate/DAO/PessoaJuridicaDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ultrabate.DAO;
+
+/**
+ *
+ * @author Denisio
+ */
+public class PessoaJuridicaDAO {
+    
+}
